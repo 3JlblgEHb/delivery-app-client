f67747c818210dab2dbdc4beb84aea6cf0a5edcf
3JlblgEHb88 <88478943+3JlblgEHb@users.noreply.github.com>
2025-04-17 11:54:26 +0300

    tailwind

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import Navigation from '@/navigation/navigation'
 import { StatusBar } from 'expo-status-bar'
 import React from 'react'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
+import "./global.css"
 
 
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,13 @@
+// tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  presets: [require('nativewind/preset')],
   content: [
-    "./App.{js,jsx,ts,tsx}",
-    "./app/**/*.{js,jsx,ts,tsx}",
-    "./src/**/*.{js,jsx,ts,tsx}",
-    "./components/**/*.{js,jsx,ts,tsx}"
+    "./App.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
